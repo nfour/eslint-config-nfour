87536e8d55b87b58d9f7cec84a70cd62a7ce71e4
nfour <novus.nfour@gmail.com>
2021-03-09 13:31:39 +1000

    Revert back to simpler config

--- .eslintrc.react.js
+++ .eslintrc.react.js
@@ -1,28 +1,18 @@
-// eslint-disable-next-line
-const base = require('./.eslintrc');
-
 /** @ts-check @type import('eslint-config-standard-typescript-prettier/types').TsEslintConfig */
 module.exports = {
-  ...base,
   rules: {
-    ...base.rules,
     'react-hooks/exhaustive-deps': ['off'],
     'react/prop-types': ['off'],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
-  plugins: [
-    ...base.plugins,
-    'eslint-plugin-react',
-    'eslint-plugin-react-hooks',
-  ],
+  plugins: ['eslint-plugin-react', 'eslint-plugin-react-hooks'],
   extends: [
-    ...base.extends,
+    './.eslintrc.js',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   parserOptions: {
-    ...base.parserOptions,
     ecmaFeatures: { jsx: true },
   },
   settings: { react: { version: 'detect' } },
