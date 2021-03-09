0b296c614d292a7e76626e3fd02d75ddcc13a34f
nfour <novus.nfour@gmail.com>
2021-03-09 13:22:18 +1000

    try another

--- .eslintrc.react.js
+++ .eslintrc.react.js
@@ -1,20 +1,28 @@
-require('@rushstack/eslint-patch/modern-module-resolution');
+// eslint-disable-next-line
+const base = require('./.eslintrc');
 
 /** @ts-check @type import('eslint-config-standard-typescript-prettier/types').TsEslintConfig */
 module.exports = {
+  ...base,
   rules: {
+    ...base.rules,
     'react-hooks/exhaustive-deps': ['off'],
     'react/prop-types': ['off'],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
-  plugins: ['eslint-plugin-react', 'eslint-plugin-react-hooks'],
+  plugins: [
+    ...base.plugins,
+    'eslint-plugin-react',
+    'eslint-plugin-react-hooks',
+  ],
   extends: [
-    require.resolve('./.eslintrc'),
+    ...base.extends,
     'eslint-config-react/recommended',
     'eslint-config-react-hooks/recommended',
   ],
   parserOptions: {
+    ...base.parserOptions,
     ecmaFeatures: { jsx: true },
   },
   settings: { react: { version: 'detect' } },
